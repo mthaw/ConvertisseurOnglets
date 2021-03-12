04e0a709259c33bc8e3029ce6453f718b5e9c6ba
mthaw <mthaw@10.0.0.11>
2021-03-11 21:24:36 -0500

    Entete

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,12 @@
 package application;
-	
+
+/*
+ * @Auteur : Martin Thaw
+ * @Date : Le 5 Mars, 2021
+ * @Description : Application (Java) qui permet d'effectuer des conversions entre des differents unites (de differents systemes; imperial, metrique, etc.). 
+ * Les 5 unites de mesure dans cet application sont le temps, la masse, la monnaie, la longueur, et la quantite de matiere.
+ */
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -12,7 +19,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Convertisseur.fxml"));
-			Scene scene = new Scene(root);
+//			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Convertisseur d'unites !"); //Mettre le titre du fenetre de l'application.
